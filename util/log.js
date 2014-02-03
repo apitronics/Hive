825372496dd5c939ecf315abc506aa1345dab733
rjsteinert <rj@rjsteinert.com>
2014-02-03 13:52:58 -0500

    can now set a context in log

--- util/log.js
+++ util/log.js
@@ -1,34 +1,37 @@
 var moment = require('moment')
-module.exports = function(a, b) {
-  static context
-  if (b != null && a = 'context') {
-    context = b
-  }
-  else { 
-    // Figure out the process we'll log as
-    var process
-    if (a && b) {
-      process = a
-    } 
-    else if (context && !b) {
-      process = context
+module.exports = {
+  context: null, 
+  g: function(a, b) {
+    if (b !== null && a == 'context') {
+      this.context = b
     }
-    else if (!context && !b) {
-      process = 'log'
-    }
+    else { 
+      // Figure out the process we'll log as
+      var context = this.context
+      var process
+      if (a && b) {
+        process = a
+      } 
+      else if (context && !b) {
+        process = context
+      }
+      else if (!context && !b) {
+        process = 'log'
+      }
 
-    // Figure out the message 
-    var msg
-    if (b == undefined) {
-      msg = a
-    }
-    else {
-      msg = b
-    }
+      // Figure out the message 
+      var msg
+      if (b == undefined) {
+        msg = a
+      }
+      else {
+        msg = b
+      }
 
-    // Log it
-    var log
-    log += process + ' -- ' + moment.utc().format() + ' -- ' + msg + ' \n'
-    console.log(log)
+      // Log it
+      var log = ''
+      log += process + ' -- ' + moment.utc().format() + ' -- ' + msg + ' \n'
+      console.log(log)
+    }
   }
 }
