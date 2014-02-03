9bda989b2173a076b9e80cda3b1522df8a88aca8
Apitronics User <users@apitronics.com>
2014-02-03 11:36:45 -0700

    More log fun

--- util/log.js
+++ util/log.js
@@ -1,12 +1,34 @@
 var moment = require('moment')
-module.exports = function(process, msg) {
-  var log = ''
-  if (msg == undefined) {
-    msg = process
-    log += 'log -- ' + moment.utc().format() + ' -- ' + msg + ' \n'
+module.exports = function(a, b) {
+  static context
+  if (b != null && a = 'context') {
+    context = b
   }
-  else {
+  else { 
+    // Figure out the process we'll log as
+    var process
+    if (a && b) {
+      process = a
+    } 
+    else if (context && !b) {
+      process = context
+    }
+    else if (!context && !b) {
+      process = 'log'
+    }
+
+    // Figure out the message 
+    var msg
+    if (b == undefined) {
+      msg = a
+    }
+    else {
+      msg = b
+    }
+
+    // Log it
+    var log
     log += process + ' -- ' + moment.utc().format() + ' -- ' + msg + ' \n'
+    console.log(log)
   }
-  console.log(log)
 }
