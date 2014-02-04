66f699fdf571643dfc73352d32865fad291ab780
rjsteinert <rj@rjsteinert.com>
2014-02-03 21:11:52 -0500

    updates for log

--- util/log.js
+++ util/log.js
@@ -1,37 +1,34 @@
 var moment = require('moment')
-module.exports = {
-  context: null, 
-  g: function(a, b) {
-    if (b !== null && a == 'context') {
-      this.context = b
+module.exports = function(a, b) {
+  if (b !== null && a == 'context') {
+    this.context = b
+  }
+  else { 
+    // Figure out the process we'll log as
+    var context = GLOBAL.logContext
+    var process
+    if (a && b) {
+      process = a
+    } 
+    else if (context && !b) {
+      process = context
     }
-    else { 
-      // Figure out the process we'll log as
-      var context = this.context
-      var process
-      if (a && b) {
-        process = a
-      } 
-      else if (context && !b) {
-        process = context
-      }
-      else if (!context && !b) {
-        process = 'log'
-      }
-
-      // Figure out the message 
-      var msg
-      if (b == undefined) {
-        msg = a
-      }
-      else {
-        msg = b
-      }
-
-      // Log it
-      var log = ''
-      log += process + ' -- ' + moment.utc().format() + ' -- ' + msg + ' \n'
-      console.log(log)
+    else if (!context && !b) {
+      process = 'log'
     }
+
+    // Figure out the message 
+    var msg
+    if (b == undefined) {
+      msg = a
+    }
+    else {
+      msg = b
+    }
+
+    // Log it
+    var log = ''
+    log += process + ' -- ' + moment.utc().format() + ' -- ' + msg + ' \n'
+    console.log(log)
   }
 }
