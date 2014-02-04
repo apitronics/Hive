61d45fcb29a247010f02ec6e39ecd4b74aaf9111
Apitronics User <users@apitronics.com>
2014-02-03 19:13:29 -0700

    dumb test

---
+++ Beekeeper/test/test--Test.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect
+
+describe("TelliTes", function() {
+  describe("#go()", function() {
+    this.timeout(15000)
+    it("info about ...", function(done) {
+      var err = null
+      var newDoc = {}
+      expect(err).to.equal(null)
+      expect(newDoc).to.be.an('object')
+      done()
+    })
+  })
+})
