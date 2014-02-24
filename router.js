7f006b2088cf89e1a367feacef7821c1052de2f6
rjsteinert <rj@rjsteinert.com>
2014-02-24 11:55:37 -0500

    Had to refactor how to build the router object because we are putting a variable in a property name

--- router.js
+++ router.js
@@ -12,13 +12,14 @@ var configDb = couchDb.db.use('config')
  * Put the App at /app/ and redirect all other requests to CouchDB.
  */
 
+var router = '{'
+router += '"' + Settings.domain + '/queen": "127.0.0.1:125",'
+router += '"' + Settings.domain + '/honeycomb":"127.0.0.1:126",'
+router += '"' + Settings.domain + '/beekeeper":"127.0.0.1:8800",'
+router += '"' + Settings.domain + '/*":"' + Settings.CouchDB.URL + '"'
+router += '}'
 var options = {
-  router: {
-    Settings.domain + '/queen': '127.0.0.1:125',
-    Settings.domain + '/honeycomb': '127.0.0.1:126',
-    Settings.domain + '/beekeeper': '127.0.0.1:8800', 
-    Settings.domain + '/*': Settings.CouchDB.URL 
-  }
+  router: JSON.parse(router)
 }
 
 httpProxy.createServer(options).listen(8000)
