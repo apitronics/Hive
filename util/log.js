6c6031487b53f764e500d88d93d4f8e55c27a038
rjsteinert <rj@rjsteinert.com>
2014-02-27 16:27:37 -0500

    Fixed time stamp in log function

--- util/log.js
+++ util/log.js
@@ -1,4 +1,4 @@
 var moment = require('moment')
 module.exports = function(process, message) {
-  console.log(moment().format('YYYY-MM-DD HH:MM:SS') + ' - ' + process + ' - ' + message)
+  console.log(moment().format('YYYY-MM-DD hh:mm:ss') + ' - ' + process + ' - ' + message)
 }
