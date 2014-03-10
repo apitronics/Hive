6a437d5985956300429b34ae17f3ad1d021ab4b7
rjsteinert <rj@rjsteinert.com>
2014-03-10 11:47:55 -0400

    removed sudo from start, not all systems have sudo

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo forever -l router.log -p /var/log/hive/ start Router/server.js  &
-sudo forever -l redirect_port_80_to_beekeeper.log -p /var/log/hive/ start Router/redirect_port_80_to_beekeeper.js &
-sudo forever -l beekeeper.log -p /var/log/hive/ start Beekeeper/server.js &
-sudo forever -l honeycomb.log -p /var/log/hive/ start Honeycomb/server.js &
-sudo forever -l queen.log -p /var/log/hive/ start Queen/server.js &
+forever -l router.log -p /var/log/hive/ start Router/server.js  &
+forever -l redirect_port_80_to_beekeeper.log -p /var/log/hive/ start Router/redirect_port_80_to_beekeeper.js &
+forever -l beekeeper.log -p /var/log/hive/ start Beekeeper/server.js &
+forever -l honeycomb.log -p /var/log/hive/ start Honeycomb/server.js &
+forever -l queen.log -p /var/log/hive/ start Queen/server.js &
