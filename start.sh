db19bf7ced640f39996ff7f877b0ae271867a63a
rjsteinert <rj@rjsteinert.com>
2014-03-10 11:36:30 -0400

    Specifying log location for start scripts

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo forever start Router/router.js &
-sudo forever start Router/redirect_port_80_to_beekeeper.js &
-sudo forever start Beekeeper/server.js &
-sudo forever start Honeycomb/server.js &
-sudo forever start Queen/server.js &
+sudo forever -l router.log -p /var/log/hive/ start Router/server.js  &
+sudo forever -l redirect_port_80_to_beekeeper.log -p /var/log/hive/ start Router/redirect_port_80_to_beekeeper.js &
+sudo forever -l beekeeper.log -p /var/log/hive/ start Beekeeper/server.js &
+sudo forever -l honeycomb.log -p /var/log/hive/ start Honeycomb/server.js &
+sudo forever -l queen.log -p /var/log/hive/ start Queen/server.js &
