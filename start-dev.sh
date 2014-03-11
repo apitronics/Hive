563d4862b4f8bd0efbdc8468d38d7625ebb815cd
rjsteinert <rj@rjsteinert.com>
2014-03-11 16:44:00 -0400

    In start-dev.sh removed a verbose option that was outputting distractions. Added couchapp sync for Beekeeper so the couchapp in apps db stays up to date while coding.

--- start-dev.sh
+++ start-dev.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-sudo forever -vdwf start Router/server.js &
-sudo forever -vdwf start Router/redirect_port_80_to_beekeeper.js &
-sudo forever -vdwf start Beekeeper/server.js &
-sudo forever -vdwf start Honeycomb/server.js &
-sudo forever -vdwf start Queen/server.js &
+sudo forever -dwf start Router/server.js &
+sudo forever -dwf start Router/redirect_port_80_to_beekeeper.js &
+sudo forever -dwf start Beekeeper/server.js &
+sudo forever -dwf start Honeycomb/server.js &
+sudo forever -dwf start Queen/server.js &
+sudo couchapp sync Beekeeper/couchapp.js http://admin:password@hive.local:5984/apps &
