e4cea6d61358af3213d4d4684d448483beca302b
Louis Thiery <me@louisthiery.com>
2014-03-18 09:18:04 -0400

    Update start.sh
    copy paste gone bad

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 cd /root/Hive/
 
-forever -a -l router.log -p /var/log/hive/ start /root/HiveRouter/server.js
-forever -a -l redirect_port_80_to_beekeeper.log -p /var/log/hive/ start Router/redirect_port_80_to_beekeeper.js
-forever -a -l beekeeper.log -p /var/log/hive/ start /root/HiveRouter/Beekeeper/server.js
-forever -a -l honeycomb.log -p /var/log/hive/ start /root/HiveRouter/Honeycomb/server.js
-forever -a -l queen.log -p /var/log/hive/ start /root/HiveRouter/Queen/server.js
+forever -a -l router.log -p /var/log/hive/ start /root/Hive/Router/server.js
+forever -a -l redirect_port_80_to_beekeeper.log -p /var/log/hive/ start root/Hive/Router/redirect_port_80_to_beekeeper.js
+forever -a -l beekeeper.log -p /var/log/hive/ start /root/Hive/Beekeeper/server.js
+forever -a -l honeycomb.log -p /var/log/hive/ start /root/Hive/Honeycomb/server.js
+forever -a -l queen.log -p /var/log/hive/ start /root/Hive/Queen/server.js
