006a9a5b58b7cd7c81a1d7cac8af7aec850dcb7a
Louis Thiery <me@louisthiery.com>
2014-04-02 11:56:50 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 cd /root/Hive/
 
 forever -a -l router.log -p /var/log/hive/ start /root/Hive/Router/server.js
-forever -a -l redirect_port_80_to_beekeeper.log -p /var/log/hive/ start root/Hive/Router/redirect_port_80_to_beekeeper.js
+forever -a -l redirect_port_80_to_beekeeper.log -p /var/log/hive/ start /root/Hive/Router/redirect_port_80_to_beekeeper.js
 forever -a -l beekeeper.log -p /var/log/hive/ start /root/Hive/Beekeeper/server.js
 forever -a -l honeycomb.log -p /var/log/hive/ start /root/Hive/Honeycomb/server.js
 forever -a -l queen.log -p /var/log/hive/ start /root/Hive/Queen/server.js
