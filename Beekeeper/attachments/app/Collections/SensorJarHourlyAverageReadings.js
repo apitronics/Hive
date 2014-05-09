d66b284da49a6be16f6ff0e9feee346f87d441c4
Hao Chen <detectx@gmail.com>
2014-05-09 16:24:39 -0400

    Fix #10 Some points are cut off from graph

--- Beekeeper/attachments/app/Collections/SensorJarHourlyAverageReadings.js
+++ Beekeeper/attachments/app/Collections/SensorJarHourlyAverageReadings.js
@@ -1,11 +1,11 @@
 $(function() {
 
   App.Collections.SensorJarHourlyAverageReadings = Backbone.Collection.extend({
-    
+
     initialize: function() {
       this.params = {
-        startDate: null, 
-        endDate: null, 
+        startDate: null,
+        endDate: null,
         sensorId: null
       }
       this.values = {}
@@ -17,10 +17,10 @@ $(function() {
         var startDate = moment(this.params.startDate.toString(), "X").format("YYYY-MM-DD HH")
         var endDate = moment(this.params.endDate.toString(), "X").format("YYYY-MM-DD HH")
         return '/sensor_' + this.params.sensorId + '__reduced__hourly-average-reduce/_all_docs'
-           + '?' 
-           + 'startkey="' + startDate + '"' 
+           + '?'
+           + 'startkey="' + startDate + '"'
            + '&'
-           + 'endkey="' + endDate + '"' 
+           + 'endkey="' + endDate + '"'
            + '&'
            + 'include_docs=true'
 
@@ -28,42 +28,33 @@ $(function() {
 
     parse: function(response) {
       // Calculate the min and max of this collection so graphing functions can take advantage of it
-      var i = 0
-      var first = 0
-      var ymin
-      var ymax
-      var docs = _.map(response.rows, function(row) {
-        row.doc.d = row.doc.value
-        if(i == 0) {
-          first = row.doc.d
+      var i = 0,
+          first = 0,
+          ymin,
+          ymax,
+          diff,
+          offset;
+
+      var docs = _.map(response.rows, function(row, index) {
+        var data = parseFloat(row.doc.value);
+        if(index === 0){
+          first = data;
+          ymin = data;
+          ymax = data;
         }
-        else if (i == 1) {
-          if (row.doc.d < first) {
-            ymin = row.doc.d
-            ymax = first
-          }
-          else {
-            ymin = first
-            ymax = row.doc.d
-          }
-        }
-        else if (i > 1) {
-          if(row.doc.d < ymin) {
-            ymin = row.doc.d
-          }
-          else if (row.doc.d > ymax) {
-            ymax = row.doc.d
-          }
-        }
-        i++
-        // Map the row object to what the Reading model expects
-        return {"_id": moment(row.doc._id, "YYYY-MM-DD HH").unix(), "d": row.doc.value}
-      })
-      this.ymin = parseInt(ymin - ((ymax-ymin)*(0.2)))
-      this.ymax = parseInt(ymax + ((ymax-ymin)*(0.2)))
-      
-      return docs
-    },
+        if(data < ymin) ymin = data;
+        if(data > ymax) ymax = data;
+        return {"_id": moment(row.doc._id, "YYYY-MM-DD HH").unix(), "d": data};
+      });
+
+      diff = ymax - ymin;
+      offset = diff * 0.2;
+
+      this.ymin = Math.floor(ymin - offset);
+      this.ymax = Math.ceil(ymax + offset);
+
+      return docs;
+    }
 
   })
 
--- Beekeeper/attachments/app/Collections/SensorReadings.js
+++ Beekeeper/attachments/app/Collections/SensorReadings.js
@@ -1,12 +1,12 @@
 $(function() {
 
   App.Collections.SensorReadings = Backbone.Collection.extend({
-    
+
     initialize: function() {
       this.params = {
         limit: null,
-        startDate: null, 
-        endDate: null, 
+        startDate: null,
+        endDate: null,
         sensorId: null
       }
     },
@@ -16,54 +16,47 @@ $(function() {
     url: function() {
       if(this.params.limit === null) {
         return '/sensor_' + this.params.sensorId + '/_all_docs'
-           + '?' 
-           + 'startkey="' + this.params.startDate + '"' 
+           + '?'
+           + 'startkey="' + this.params.startDate + '"'
            + '&'
-           + 'endkey="' + this.params.endDate + '"' 
+           + 'endkey="' + this.params.endDate + '"'
            + '&'
            + 'include_docs=true'//&reduce_to=100'
       }
       else {
-        return '/sensor_' + this.params.sensorId + '/_all_docs' + '?startkey="FFFFF"&endkey=""&limit=1&descending=true&include_docs=true' 
+        return '/sensor_' + this.params.sensorId + '/_all_docs' + '?startkey="FFFFF"&endkey=""&limit=1&descending=true&include_docs=true'
       }
     },
 
     parse: function(response) {
       // Calculate the min and max of this collection so graphing functions can take advantage of it
-      var i = 0
-      var first = 0
-      var ymin
-      var ymax
-      var docs = _.map(response.rows, function(row) {
-        if(i == 0) {
-          first = row.doc.d
+      var i = 0,
+          first = 0,
+          ymin,
+          ymax,
+          diff,
+          offset;
+
+      var docs = _.map(response.rows, function(row, index) {
+        var data = parseFloat(row.doc.d);
+        if(index === 0){
+          first = data;
+          ymin = data;
+          ymax = data;
         }
-        else if (i == 1) {
-          if (row.doc.d < first) {
-            ymin = row.doc.d
-            ymax = first
-          }
-          else {
-            ymin = first
-            ymax = row.doc.d
-          }
-        }
-        else if (i > 1) {
-          if(row.doc.d < ymin) {
-            ymin = row.doc.d
-          }
-          else if (row.doc.d > ymax) {
-            ymax = row.doc.d
-          }
-        }
-        i++
-        return row.doc
-      })
-      this.ymin = parseInt(ymin - ((ymax-ymin)*(0.2)))
-      this.ymax = parseInt(ymax + ((ymax-ymin)*(0.2)))
-      
-      return docs
-    },
+        if(data < ymin) ymin = data;
+        if(data > ymax) ymax = data;
+        return row.doc;
+      });
+
+      diff = ymax - ymin;
+      offset = diff * 0.2;
+
+      this.ymin = Math.floor(ymin - offset);
+      this.ymax = Math.ceil(ymax + offset);
+
+      return docs;
+    }
 
   })
 
