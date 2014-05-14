97290b4b192d975bfc72d9026212d8ecf83c2a62
Hao Chen <detectx@gmail.com>
2014-05-14 15:06:44 -0400

    Add confirmation to update button, stylize

--- Beekeeper/attachments/app/Views/HiveUpdates.js
+++ Beekeeper/attachments/app/Views/HiveUpdates.js
@@ -7,20 +7,30 @@ $(function() {
       this.model.bind('change', this.render);
     },
 
+    events: {
+      'click .hive-updates-available': 'confirmUpdate'
+    },
+
     template : _.template($('#template-HiveUpdates').html()),
 
     render: function () {
-      vars = {}
-      var updates = this.model.get('updates')
+      var vars = {},
+          updates = this.model.get('updates');
+
       if (updates.length > 0) {
-        vars.info = 'Update Hive (' + updates.length + ')'
-        this.$el.html(this.template(vars))
+        vars.info = 'Update Hive (' + updates.length + ')';
+        this.$el.html(this.template(vars));
       }
       else {
-        this.$el.html()
+        this.$el.html();
       }
     },
 
-  })
+    confirmUpdate: function() {
+      return confirm('Are you sure you want to download the latest codebase and update the Hive?');
+    }
 
-})
+
+  });
+
+});
